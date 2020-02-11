db8d39e9736423ca2b918c682c81620ff0de4989
Jupisoft <jupitermauro@gmail.com>
2020-02-11 01:56:44 +0100

    Create _Todo_.cs

---
+++ Minecraft Tools/_Todo_.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Tools
+{
+    /// <summary>
+    /// This just a list of ideas to do in a near future:
+    /// 
+    /// [STARTED] Virtual moon.
+    /// Resource extractor.
+    /// Caleidoscope.
+    /// Character map.
+    /// Auto-hotkeys, to let the mouse buttons pressed, for AFK in Minecraft, etc.
+    /// Note block auto-tuner, to record audio and tell it's note.
+    /// Minecraft recipe guide, directly read the recipe files and load the results.
+    /// Morse code translator, even from WAV or audio files, use ffmpeg.
+    /// DDS texture file reader.
+    /// Stars screen saver.
+    /// Obtain a text from any world seed number that actually gives that seed number.
+    /// Add a tool with Jupisoft downloads with things for Minecraft like worlds, resource packs...
+    /// Add more filters to the real time screen filters.
+    /// Add a dictionary of regions to draw underground caves maps this will save RAM for null regions.
+    /// Add grass and all flowers to the underground caves dictionary on it's 2 functions to avoid floating ones.
+    /// Count how many tools each group has and show it in the tool selector as text.
+    /// Add an image to show the recipes of all the potions like the one with the villager tradings.
+    /// 
+    /// [DONE] 1.13+ to 1.12.2- world converter and structure exporter doesn't save the wood slabs properly.
+    /// 
+    /// Bug when drawing in the SGA the same text but either in upper or lowercase.
+    /// 
+    /// Slime chunk finder wrongly converts to chunk and possibly region the negative coordinates.
+    /// 
+    /// 
+    /// </summary>
+    internal static class _Todo_
+    {
+
+    }
+}
