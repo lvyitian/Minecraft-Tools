2a4081747e9207b5ce815116719baeaa4a76b0a5
Jupisoft <jupitermauro@gmail.com>
2020-02-11 01:56:46 +0100

    Create Analizador_Matemático_Multidimensional.cs

---
+++ Minecraft Tools/Analizador_Matemático_Multidimensional.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Tools
+{
+    internal static class Analizador_Matemático_Multidimensional
+    {
+        internal static readonly int[][] Matriz_Base_2_Potencias_Valores = new int[][]
+        {
+            
+        };
+
+        /*internal static readonly int[][] Matriz_Base_3_Potencias_Valores = new int[12][]
+        {
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { }
+        };
+
+        internal static readonly int[][] Matriz_Base_4_Potencias_Valores = new int[12][]
+        {
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { }
+        };
+
+        internal static readonly int[][] Matriz_Base_5_Potencias_Valores = new int[12][]
+        {
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+
+            new int[576] { },
+            new int[576] { },
+            new int[576] { },
+            new int[576] { }
+        };*/
+    }
+}
