566a21ec1d41413b15e0477b1fcc31f23a976818
Jupisoft <jupitermauro@gmail.com>
2020-02-11 08:52:40 +0100

    Create Ventana_Descargador_Skins_Automático.cs

---
+++ Minecraft Tools/Ventana_Descargador_Skins_Automático.cs
@@ -0,0 +1,213 @@
+﻿using Minecraft_Tools.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Tools
+{
+    public partial class Ventana_Descargador_Skins_Automático: Form
+    {
+        public Ventana_Descargador_Skins_Automático()
+        {
+            InitializeComponent();
+        }
+
+        internal static bool Variable_Descargar_Skins = false;
+        internal static string Variable_Nombre = null;
+        internal static string Variable_Juego_Caracteres = null;
+
+        internal readonly string Texto_Título = "Automatic Skins Downloader by Jupisoft for " + Program.Texto_Usuario;
+        internal bool Variable_Siempre_Visible = false;
+        internal bool Variable_Excepción = false;
+        internal bool Variable_Excepción_Imagen = false;
+        internal int Variable_Excepción_Total = 0;
+        internal bool Variable_Memoria = false;
+
+        // string.Format("http://skins.minecraft.net/MinecraftSkins/{0}.png", Nombre)
+
+        private void Ventana_Descargador_Skins_Automático_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Icon = Program.Icono_Jupisoft.Clone() as Icon;
+                this.Text = Texto_Título;
+                this.WindowState = FormWindowState.Maximized;
+                TextBox_Nombre.Text = Variable_Nombre;
+                if (string.IsNullOrEmpty(Variable_Juego_Caracteres) && ComboBox_Juego_Caracteres.Items.Count > 0) ComboBox_Juego_Caracteres.SelectedIndex = ComboBox_Juego_Caracteres.Items.Count - 1;
+                else ComboBox_Juego_Caracteres.Text = Variable_Juego_Caracteres;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Ventana_Descargador_Skins_Automático_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Activate();
+                Temporizador_Principal.Start();
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Ventana_Descargador_Skins_Automático_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Temporizador_Principal.Stop();
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Ventana_Descargador_Skins_Automático_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Ventana_Descargador_Skins_Automático_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!e.Alt && !e.Control && !e.Shift)
+                {
+                    if (e.KeyCode == Keys.Escape || e.KeyCode == Keys.Delete)
+                    {
+                        e.Handled = true;
+                        e.SuppressKeyPress = true;
+                        this.Close();
+                    }
+                }
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void CheckBox_Descargar_Skins_CheckedChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Variable_Descargar_Skins = CheckBox_Descargar_Skins.Checked;
+
+                // soon...
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void TextBox_Nombre_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                Variable_Nombre = TextBox_Nombre.Text;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void ComboBox_Juego_Caracteres_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(ComboBox_Juego_Caracteres.Text)) Variable_Juego_Caracteres = ComboBox_Juego_Caracteres.Text;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Barra_Estado_Botón_Excepción_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Variable_Excepción = false;
+                Variable_Excepción_Imagen = false;
+                Variable_Excepción_Total = 0;
+                Barra_Estado_Botón_Excepción.Visible = false;
+                Barra_Estado_Separador_1.Visible = false;
+                Barra_Estado_Botón_Excepción.Image = Resources.Excepción_Gris;
+                Barra_Estado_Botón_Excepción.ForeColor = Color.Black;
+                Barra_Estado_Botón_Excepción.Text = "Exceptions: 0";
+                Ventana_Depurador_Excepciones Ventana = new Ventana_Depurador_Excepciones();
+                Ventana.ShowDialog(this);
+                Ventana.Dispose();
+                Ventana = null;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Temporizador_Principal_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                if (Variable_Excepción)
+                {
+                    if ((Environment.TickCount / 500) % 2 == 0)
+                    {
+                        if (!Variable_Excepción_Imagen)
+                        {
+                            Variable_Excepción_Imagen = true;
+                            Barra_Estado_Botón_Excepción.Image = Resources.Excepción;
+                            Barra_Estado_Botón_Excepción.ForeColor = Color.Red;
+                            Barra_Estado_Botón_Excepción.Text = "Exceptions: " + Program.Traducir_Número(Variable_Excepción_Total);
+                        }
+                    }
+                    else
+                    {
+                        if (Variable_Excepción_Imagen)
+                        {
+                            Variable_Excepción_Imagen = false;
+                            Barra_Estado_Botón_Excepción.Image = Resources.Excepción_Gris;
+                            Barra_Estado_Botón_Excepción.ForeColor = Color.Black;
+                            Barra_Estado_Botón_Excepción.Text = "Exceptions: " + Program.Traducir_Número(Variable_Excepción_Total);
+                        }
+                    }
+                    if (!Barra_Estado_Botón_Excepción.Visible) Barra_Estado_Botón_Excepción.Visible = true;
+                    if (!Barra_Estado_Separador_1.Visible) Barra_Estado_Separador_1.Visible = true;
+                }
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+            try
+            {
+                try
+                {
+                    Program.Proceso.Refresh();
+                    long Memoria_Bytes = Program.Proceso.PagedMemorySize64;
+                    Barra_Estado_Etiqueta_Memoria.Text = "RAM: " + Program.Traducir_Tamaño_Bytes_Automático(Memoria_Bytes, 2, true);
+                    if (Memoria_Bytes < 4294967296L) // < 4 GB
+                    {
+                        if (Variable_Memoria)
+                        {
+                            Variable_Memoria = false;
+                            Barra_Estado_Etiqueta_Memoria.ForeColor = Color.Black;
+                        }
+                    }
+                    else // >= 4 GB
+                    {
+                        if ((Environment.TickCount / 500) % 2 == 0)
+                        {
+                            if (!Variable_Memoria)
+                            {
+                                Variable_Memoria = true;
+                                Barra_Estado_Etiqueta_Memoria.ForeColor = Color.Red;
+                            }
+                        }
+                        else
+                        {
+                            if (Variable_Memoria)
+                            {
+                                Variable_Memoria = false;
+                                Barra_Estado_Etiqueta_Memoria.ForeColor = Color.Black;
+                            }
+                        }
+                    }
+                }
+                catch { Barra_Estado_Etiqueta_Memoria.Text = "RAM: ? MB (? GB)"; }
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+    }
+}
