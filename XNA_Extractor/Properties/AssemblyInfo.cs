fdfa34e42b2e03181f0eca062141050974f8b337
Jupisoft <jupitermauro@gmail.com>
2020-02-11 09:09:22 +0100

    Create AssemblyInfo.cs

---
+++ XNA_Extractor/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("XNA Extractor")]
+[assembly: AssemblyDescription("Only for the Glory of God")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Jupisoft")]
+[assembly: AssemblyProduct("XNA Extractor by Jupisoft")]
+[assembly: AssemblyCopyright("© 2004 - 2019 by Jupisoft, all rights reserved")]
+[assembly: AssemblyTrademark("Jupisoft")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("837f8816-fcf9-4925-916f-ac2c36cf4def")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o utilizar los números de compilación y de revisión predeterminados
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
