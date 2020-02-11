a3184575cf8b06c97ccc3304f2d0bed1ee5c5b64
Jupisoft <jupitermauro@gmail.com>
2020-02-11 08:59:26 +0100

    Create Ventana_Información_Bloques.Designer.cs

---
+++ Minecraft Tools/Ventana_Información_Bloques.Designer.cs
@@ -0,0 +1,55 @@
+﻿namespace Minecraft_Tools
+{
+    partial class Ventana_Información_Bloques
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Ventana_Información_Bloques
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 461);
+            this.DoubleBuffered = true;
+            this.Name = "Ventana_Información_Bloques";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Block Information by Jupisoft";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Ventana_Información_Bloques_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ventana_Información_Bloques_FormClosed);
+            this.Load += new System.EventHandler(this.Ventana_Información_Bloques_Load);
+            this.Shown += new System.EventHandler(this.Ventana_Información_Bloques_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Ventana_Información_Bloques_KeyDown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
