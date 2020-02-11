adc7e08c96eaa03120202c4331d562adbc4b4827
Jupisoft <jupitermauro@gmail.com>
2020-02-11 09:06:53 +0100

    Create Ventana_Visor_Partituras_Júpiter_Mauro.cs

---
+++ Minecraft Tools/Ventana_Visor_Partituras_Júpiter_Mauro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Minecraft_Tools
+{
+    /// <summary>
+    /// Empty place holder to use instead of the old window with all the scores, etc.
+    /// </summary>
+    internal static class Ventana_Visor_Partituras_Júpiter_Mauro
+    {
+
+    }
+}
