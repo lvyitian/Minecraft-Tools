0b94b48ce532fdbfa6bf94ee6422d88a293b4086
Jupisoft <jupitermauro@gmail.com>
2020-02-11 09:07:28 +0100

    Create Ventana_Visor_Skins_Animado_3D.Designer.cs

---
+++ Minecraft Tools/Ventana_Visor_Skins_Animado_3D.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace Minecraft_Tools
+{
+    partial class Ventana_Visor_Skins_Animado_3D
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox32 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.Grupo_Skin = new System.Windows.Forms.GroupBox();
+            this.CheckedListBox_Skin = new System.Windows.Forms.CheckedListBox();
+            this.Grupo_Skin.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox32
+            // 
+            this.textBox32.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox32.BackColor = System.Drawing.Color.White;
+            this.textBox32.Enabled = false;
+            this.textBox32.Location = new System.Drawing.Point(434, 19);
+            this.textBox32.Name = "textBox32";
+            this.textBox32.ReadOnly = true;
+            this.textBox32.Size = new System.Drawing.Size(20, 20);
+            this.textBox32.TabIndex = 1;
+            this.textBox32.Visible = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(434, 45);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(20, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Visible = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(434, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(20, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Visible = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.Enabled = false;
+            this.textBox3.Location = new System.Drawing.Point(434, 97);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(20, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Visible = false;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox4.BackColor = System.Drawing.Color.White;
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(434, 123);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(20, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Visible = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox5.BackColor = System.Drawing.Color.White;
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(434, 149);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(20, 20);
+            this.textBox5.TabIndex = 6;
+            this.textBox5.Visible = false;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox6.BackColor = System.Drawing.Color.White;
+            this.textBox6.Enabled = false;
+            this.textBox6.Location = new System.Drawing.Point(434, 175);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(20, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.Visible = false;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox7.BackColor = System.Drawing.Color.White;
+            this.textBox7.Enabled = false;
+            this.textBox7.Location = new System.Drawing.Point(434, 201);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(20, 20);
+            this.textBox7.TabIndex = 8;
+            this.textBox7.Visible = false;
+            // 
+            // Grupo_Skin
+            // 
+            this.Grupo_Skin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Grupo_Skin.Controls.Add(this.textBox7);
+            this.Grupo_Skin.Controls.Add(this.textBox32);
+            this.Grupo_Skin.Controls.Add(this.textBox6);
+            this.Grupo_Skin.Controls.Add(this.textBox1);
+            this.Grupo_Skin.Controls.Add(this.textBox5);
+            this.Grupo_Skin.Controls.Add(this.textBox2);
+            this.Grupo_Skin.Controls.Add(this.textBox4);
+            this.Grupo_Skin.Controls.Add(this.textBox3);
+            this.Grupo_Skin.Controls.Add(this.CheckedListBox_Skin);
+            this.Grupo_Skin.Location = new System.Drawing.Point(12, 12);
+            this.Grupo_Skin.Name = "Grupo_Skin";
+            this.Grupo_Skin.Size = new System.Drawing.Size(460, 264);
+            this.Grupo_Skin.TabIndex = 0;
+            this.Grupo_Skin.TabStop = false;
+            this.Grupo_Skin.Text = "Skin options - [drag and drop any image with a Minecraft player skin]";
+            // 
+            // CheckedListBox_Skin
+            // 
+            this.CheckedListBox_Skin.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.CheckedListBox_Skin.BackColor = System.Drawing.Color.White;
+            this.CheckedListBox_Skin.FormattingEnabled = true;
+            this.CheckedListBox_Skin.IntegralHeight = false;
+            this.CheckedListBox_Skin.Items.AddRange(new object[] {
+            "Show head",
+            "Show body",
+            "Show left arm",
+            "Show right arm",
+            "Show left leg",
+            "Show right leg",
+            "Show cape",
+            "Show hair",
+            "Show jacket",
+            "Show pants",
+            "Show nude skin",
+            "Show inverted chin",
+            "Show head mask"});
+            this.CheckedListBox_Skin.Location = new System.Drawing.Point(6, 19);
+            this.CheckedListBox_Skin.Name = "CheckedListBox_Skin";
+            this.CheckedListBox_Skin.ScrollAlwaysVisible = true;
+            this.CheckedListBox_Skin.Size = new System.Drawing.Size(448, 202);
+            this.CheckedListBox_Skin.TabIndex = 0;
+            this.CheckedListBox_Skin.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Ventana_Visor_Skins_Animado_3D_KeyDown);
+            // 
+            // Ventana_Visor_Skins_Animado_3D
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.Grupo_Skin);
+            this.DoubleBuffered = true;
+            this.Name = "Ventana_Visor_Skins_Animado_3D";
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Animated 3D Skin Viewer by Jupisoft";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Ventana_Visor_Skins_Animado_3D_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Ventana_Visor_Skins_Animado_3D_FormClosed);
+            this.Load += new System.EventHandler(this.Ventana_Visor_Skins_Animado_3D_Load);
+            this.Shown += new System.EventHandler(this.Ventana_Visor_Skins_Animado_3D_Shown);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Ventana_Visor_Skins_Animado_3D_KeyDown);
+            this.Grupo_Skin.ResumeLayout(false);
+            this.Grupo_Skin.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox32;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.GroupBox Grupo_Skin;
+        private System.Windows.Forms.CheckedListBox CheckedListBox_Skin;
+    }
+}
