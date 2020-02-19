2d80e819d9fd9104f1abf40ac8c3bd380ed06a83
Jupisoft <jupitermauro@gmail.com>
2020-02-19 01:33:21 +0100

    Update AssemblyInfo.cs

--- Minecraft Tools/Properties/AssemblyInfo.cs
+++ Minecraft Tools/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jupisoft")]
 [assembly: AssemblyProduct("Minecraft Tools by Jupisoft")]
-[assembly: AssemblyCopyright("© 2004 - 2019 by Jupisoft, all rights reserved")]
+[assembly: AssemblyCopyright("© 2004 - 2020 by Jupisoft, all rights reserved")]
 [assembly: AssemblyTrademark("Jupisoft")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.15.0.0")]
-[assembly: AssemblyFileVersion("1.15.0.0")]
+[assembly: AssemblyVersion("1.16.0.0")]
+[assembly: AssemblyFileVersion("1.16.0.0")]
